a69d67fb5a043157924ba948e01793467c162153
LisaHollaus <23IMC10611@fh-krems.ac.at>
2024-11-03 15:06:07 +0100

    basic css structure

--- AFS_Group1/settings.py
+++ AFS_Group1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "floodproject",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'AFS_Group1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'floodproject/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
