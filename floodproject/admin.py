7aba38f1c7df1982c4ce3e2e020c3c9e847aa2d9
Erwin Lenart <erwin.lenart@yahoo.com>
2024-11-13 11:14:09 +0100

    create custom table in admin page

--- floodproject/admin.py
+++ floodproject/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from .models import CustomUser
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    model = CustomUser
+
+    # Fields to display in the list view
+    list_display = ('email', 'first_name', 'last_name', 'zip_code', 'city', 'role', 'is_staff', 'is_superuser', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'role')
+
+    # Fields displayed in the detail view for existing users
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'zip_code', 'city', 'role')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+
+    # Fields displayed in the create user form
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'zip_code', 'city', 'role', 'is_staff', 'is_active')
+        }),
+    )
+
+    # Configuration for search and ordering
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions')
+
+# Register the custom user model and unregister the Group model
+admin.site.register(CustomUser, UserAdmin)
+admin.site.unregister(Group)
--- floodproject/models.py
+++ floodproject/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, zip_code, city, password=None, role = "user"):
+        user = self.model(email = email, first_name = first_name, last_name = last_name, zip_code = zip_code, city = city, role = role)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+    
+    def create_superuser(self, email, first_name, last_name, zip_code, city, password=None, role = "admin"):
+        user = self.create_user(email = email, first_name = first_name, last_name = last_name, zip_code = zip_code, city = city, role = role)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=50)
+    role = models.CharField(max_length=20, default="user")
+    
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'zip_code', 'city']
+
+    def __str__(self):
+        return self.email
