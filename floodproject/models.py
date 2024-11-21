04548926408b7f2345bc2ef0cb585b1729412696
Christian-Hummel <23imc10881@fh-krems.ac.at>
2024-11-21 23:34:08 +0100

    created schema for waterlevels

--- floodproject/models.py
+++ floodproject/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 
+class WaterLevel(models.Model):
+    measuring_point =  models.CharField(max_length=256)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    value = models.FloatField()
+    unit = models.CharField(max_length=256)
+
+    def __str__self(self):
+        return self.measuring_point
