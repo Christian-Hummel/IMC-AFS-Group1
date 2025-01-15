1df599323741af4588705e2035fc1a2d8ac3ebe4
Christian-Hummel <115420636+Christian-Hummel@users.noreply.github.com>
2025-01-15 14:26:39 +0100

    added asynchronous function for commenting, added function to handle switching tabs functionality

--- floodproject/static/js/report.js
+++ floodproject/static/js/report.js
@@ -1,5 +1,5 @@
 
-
+var tempflag = false
 // error if geolocation is not possible - does not check for denied permission, only the possibility
 if (!navigator.geolocation) {
     throw new Error("No geolocation available");
@@ -13,6 +13,50 @@ function showVotestats(){
     } else {
         x.style.display = "none"
     }
+}
+
+function toggleSubscribe(){
+    var request = new XMLHttpRequest();
+
+    request.open('GET', subscribeUrl);
+
+    request.send();
+
+    setTimeout(() => {
+        window.location.reload();
+    }, 10)
+
+}
+
+
+function toggleTabs (target){
+
+    const details = document.getElementById("details");
+    const comments = document.getElementById("comments");
+    const tasks = document.getElementById("tasks");
+
+    // create a list of all tabs and iterate over it
+    const array = [details, comments, tasks]
+
+    array.forEach((element) => {
+        if (target !== element.id){
+            element.style.display = "none"
+            // if condition to check if a button has got the class "active" - is highlighted
+            if (document.getElementById(element.id.concat("button")).classList.contains("active")){
+                document.getElementById(element.id.concat("button")).classList.remove("active")
+            }
+        }
+
+    })
+
+    // make target div visible
+    document.getElementById(target).style.display = "block"
+    // highlight corresponding tab button
+    document.getElementById(target.concat("button")).classList.add("active")
+
+
+
+
 }
 
 
@@ -102,21 +146,68 @@ function computeDistance(location) {
         // change status of submit button
         vsubmit.removeAttribute("hidden")
 
-
-
-        // console.log("report details", repdetails)
-        // console.log("reportvote", reportvote)
-
-
-
     }
 
-
-
-
-
-
-
 }
+
+jQuery(document).on('submit', '#submitcomment', function(e){
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: submitUrl,
+            data: {
+                textcomment: $('#textcomment').val(),
+                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+            },
+            success: function (data) {
+                // extract raw value from hidden input field - comments.val() to extract value as string
+                var comments = $("#ccount");
+
+                var userId = JSON.parse(document.getElementById('user_id').textContent)
+                var subscriptions = JSON.parse(document.getElementById('subscriptions').textContent)
+
+                var unsubscribe = document.getElementById("unsubscribe")
+                var subscribe = document.getElementById("subscribe")
+
+                if (subscriptions.indexOf(userId) < 0 && tempflag === false){
+
+
+                    var request = new XMLHttpRequest();
+
+                    request.open('GET', subscribeUrl);
+
+                    request.send();
+
+                    unsubscribe.style.display = ""
+                    subscribe.style.display = "none"
+
+                    tempflag = true
+
+                }
+
+                // if there are no comments yet
+                if (Number(comments.val()) === 0){
+                    // add comment to the end of div with id comments
+                    $('.comments').append(data);
+
+                    //increment value -> +1
+
+                    comments.val(parseInt(comments.val()+1));
+
+                    // remove error message from the document
+                    var commenterror = document.getElementById("comment-error")
+                    commenterror.style.display = "none"
+
+                    // if there are already comments, increment hidden input value and add comment as first element
+                } else {
+                    $('.comments').find('.comment').first().prepend(data);
+                    comments.val( parseInt(comments.val())+1);
+                }
+                $("#textcomment").val('');
+            }
+
+        })
+})
+
 
 
